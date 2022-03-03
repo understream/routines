5941407e507fc5f6b12f7a3696ef4e0581f5225d
understream <me@nowhere.com>
2022-03-03 11:33:34 +0800

    add

---
+++ python3/csv2json.py
@@ -0,0 +1,38 @@
+#encoding=utf-8
+import sys
+import json
+
+def csv2json( csv_path ):
+    yield "["
+    with open(csv_path, "r", encoding='utf-8') as f:
+        columns = f.readline().strip().split(",")
+        while True:
+            l = f.readline()
+            if not l.strip(): break
+            ll = l.strip().split(",")
+            tmp = {}
+            for i in range(0, max(len(columns), len(ll))):
+                if i < len(columns) and i < len(ll):
+                    tmp[ columns[i] ] = ll[i]
+                elif i < len(columns):
+                    tmp[ columns[i] ] = None 
+                elif i < len(ll):
+                    tmp[ "$key_%d"%(i+1) ] = ll[i]
+            yield json.dumps( tmp ) + ","
+
+    yield "]"
+
+
+
+
+        
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python %s <csv_file>" % sys.argv[0])
+        sys.exit(-1)
+
+    for r in csv2json( sys.argv[1] ):
+        print( r )
+
+    
